4eb3e1790c80925723408f3295abc7690d3c2c1f
amitksingh1490 <amitksingh1490@users.noreply.github.com>
2025-02-25 12:08:13 +0530

    remove unused files and configurations; add new components and styles for improved layout and functionality

---
+++ src/components/home/Banner.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import Heading from "@theme/Heading"
+
+import LinkButton from "../shared/LinkButton"
+import {analyticsHandler} from "@site/src/utils"
+import {Theme, codeSandboxUrl} from "@site/src/constants"
+import {pageLinks} from "@site/src/constants/routes"
+import Section from "../shared/Section"
+
+const Banner = (): JSX.Element => {
+  return (
+    <main className="grid justify-center">
+      <Section className="flex flex-col sm:items-center sm:text-center w-full !pb-0">
+        <div className="h-full 2xl:min-h-0">
+          <Heading
+            as="h1"
+            className="hero-banner-title text-title-large max-w-xs sm:text-display-small lg:text-display-large sm:max-w-5xl"
+          >
+            Code Smarter. <br />
+            <span className="bg-tailCall-yellow rounded-md sm:rounded-2xl px-SPACE_02">Build Faster.</span> Rule the Terminal.
+          </Heading>
+          <p className="hero-banner-sub-title sm:max-w-2xl sm:m-auto text-content-small sm:text-content-medium lg:text-content-large font-normal max-w-md sm:mt-SPACE_04 mb-0">
+          Unleash your potential with Code-Forge, the AI coding agent that transforms senior developers into 100x engineers. Command the terminal with unprecedented power.
+          </p>
+          <div className="hidden sm:flex justify-center mt-SPACE_06 sm:mt-SPACE_10 space-x-SPACE_04 sm:space-x-SPACE_06">
+            <LinkButton
+              title="Get Started"
+              href={pageLinks.docs}
+              theme={Theme.Light}
+              width="small"
+              onClick={() => analyticsHandler("Home Page", "Click", "Get Started")}
+            />
+          </div>
+
+          <div className="sm:hidden flex justify-between md:justify-center mt-SPACE_06 sm:mt-SPACE_10 space-x-SPACE_04 sm:space-x-SPACE_06">
+            <LinkButton
+              title="Get Started"
+              href={pageLinks.docs}
+              theme={Theme.Light}
+              onClick={() => analyticsHandler("Home Page", "Click", "Get Started")}
+              width="full"
+            />
+          </div>
+        </div>
+      </Section>
+    </main>
+  )
+}
+
+export default Banner
