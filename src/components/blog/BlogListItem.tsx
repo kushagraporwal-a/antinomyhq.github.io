cf83a012ba0d5039b5d03ffa2aa3683bdf6cdc79
Kushagra Porwal <v-kushagra.porwal@rksv.in>
2025-07-04 16:14:02 +0530

    added changes for theme on Blog listing page

--- src/components/blog/BlogListItem.tsx
+++ src/components/blog/BlogListItem.tsx
@@ -15,8 +15,8 @@ export interface BlogListItemProps {
 const BlogListItem: React.FC<BlogListItemProps> = ({date, title, description, authors, permalink}) => {
   return (
     <Link to={permalink} className="flex flex-col overflow-hidden !text-black !no-underline">
-      <div className="flex flex-col flex-1 p-3 md:py-12 md:px-6 gap-2 md:gap-3 border border-dashed border-tailCall-border-light-600 hover:border-tailCall-border-dark-100">
-        <span className="hidden md:flex text-content-mini text-black">
+      <div className="flex flex-col flex-1 p-3 md:py-12 md:px-6 gap-2 md:gap-3 border border-dashed border-tailCall-lightMode---primary-400 dark:border-tailCall-darkMode---primary-400">
+        <span className="hidden md:flex text-content-mini text-tailCall-lightMode---neutral-500 dark:text-tailCall-darkMode---neutral-500">
           {new Date(date).toLocaleDateString("en-US", {
             month: "long",
             day: "numeric",
@@ -25,12 +25,12 @@ const BlogListItem: React.FC<BlogListItemProps> = ({date, title, description, au
           })}
         </span>
         <div className="flex flex-col flex-1 gap-1 md:gap-2">
-          <span className={clsx("text-title-small line-clamp-2")}>{title}</span>
-          <span className="flex-1 text-content-tiny md:text-content-small line-clamp-1 md:line-clamp-3 text-tailCall-light-800 blog-post-content-desc">
+          <span className={clsx("text-title-small line-clamp-2 text-tailCall-lightMode---primary-300 dark:text-tailCall-darkMode---primary-300")}>{title}</span>
+          <span className="flex-1 text-content-tiny md:text-content-small line-clamp-1 md:line-clamp-3 text-tailCall-lightMode---neutral-600 dark:text-tailCall-darkMode---neutral-600 blog-post-content-desc">
             {description}
           </span>
         </div>
-        {authors[0] && <BlogAuthor author={authors[0]} containerClassName="mt-auto" />}
+        {authors[0] && <BlogAuthor author={authors[0]} containerClassName="mt-auto" textClassName="text-tailCall-lightMode---neutral-400 dark:text-tailCall-darkMode---neutral-400" />}
       </div>
     </Link>
   )
--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -12,20 +12,22 @@ export default function BlogLayout(props: Props): JSX.Element {
 
   return (
     <Layout {...layoutProps}>
-      <div className="container mx-auto mt-3 mb-10 md:my-8 px-4 ">
-        <div className="flex flex-row">
-          {isBlogPostPage && <div className="hidden lg:block lg:w-[20.83%]"></div>}
-          <div className={clsx("w-full", isBlogPostPage && "lg:w-[58.33%]")}>{children}</div>
-          {toc && (
-            <div className="hidden lg:block lg:w-[20.83%] pl-8 mt-12">
-              <div className="sticky top-28">
-                <div className="border-l-solid border-gray-200 pl-4">{toc}</div>
+      <div className="bg-white dark:bg-black min-h-screen w-full">
+        <div className="container mx-auto mt-3 mb-10 md:my-8 px-4 ">
+          <div className="flex flex-row">
+            {isBlogPostPage && <div className="hidden lg:block lg:w-[20.83%]"></div>}
+            <div className={clsx("w-full", isBlogPostPage && "lg:w-[58.33%]")}>{children}</div>
+            {toc && (
+              <div className="hidden lg:block lg:w-[20.83%] pl-8 mt-12">
+                <div className="sticky top-28">
+                  <div className="border-l-solid border-gray-200 pl-4">{toc}</div>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
+        <BlogRecentPosts sidebar={sidebar} />
       </div>
-      <BlogRecentPosts sidebar={sidebar} />
     </Layout>
   )
 }
