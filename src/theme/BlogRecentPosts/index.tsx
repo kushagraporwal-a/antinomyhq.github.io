e80e3d548da7ad22c43cee8a0450f2f2e7c66353
kushagraporwal-a <kushagra.porwal@apptware.com>
2025-08-01 14:06:52 +0530

    feat: add illustration images on home page

--- src/theme/BlogRecentPosts/index.tsx
+++ src/theme/BlogRecentPosts/index.tsx
@@ -4,6 +4,7 @@ import type {Props} from "@theme/BlogLayout"
 import {isBlogPost} from "@site/src/utils"
 import {usePluginData} from "@docusaurus/useGlobalData"
 import BlogListItem from "@site/src/components/blog/BlogListItem"
+import {assignBgIndices} from "@site/src/utils/blogColorUtils"
 
 export default function BlogRecentPosts({sidebar}: {sidebar: Props["sidebar"]}): JSX.Element {
   const [isBlogPostPage, setIsBlogPostPage] = React.useState(false)
@@ -15,7 +16,8 @@ export default function BlogRecentPosts({sidebar}: {sidebar: Props["sidebar"]}):
     setIsBlogPostPage(isBlogPost())
   }, [location.pathname])
 
-  console.log(recentBlogPostsMetadata)
+  const permalink = recentBlogPostsMetadata?.slice(0, 3).map((item: RecentBlogPostItem) => item.permalink)
+  const blogColorMap = assignBgIndices(permalink)
 
   return isBlogPostPage ? (
     <div className="">
@@ -28,7 +30,7 @@ export default function BlogRecentPosts({sidebar}: {sidebar: Props["sidebar"]}):
           <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 md:gap-3 mb-10 md:mb-20">
             {recentBlogPostsMetadata?.slice(0, 3).map((item: RecentBlogPostItem) => {
               const {permalink, date, title, description, authors, tags} = item
-              const randomNumber = Math.floor(Math.random() * 6)
+              const bgIndex = blogColorMap[permalink] ?? 0
               return (
                 <BlogListItem
                   key={permalink}
@@ -38,7 +40,7 @@ export default function BlogRecentPosts({sidebar}: {sidebar: Props["sidebar"]}):
                   authors={authors}
                   permalink={permalink}
                   tags={tags}
-                  bgIndex={randomNumber}
+                  bgIndex={bgIndex}
                 />
               )
             })}
